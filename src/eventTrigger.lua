db07ac7ebd4fc92fa3e1f9cc796393687ff7457b
Racorror <paul.fluck@t-online.de>
2017-10-28 15:17:07 +0200

    Add files via upload

--- src/eventTrigger.lua
+++ src/eventTrigger.lua
@@ -1,8 +1,54 @@
-function triggerEvents()
+function triggerEventsLevel1()
+  if playerX == elkX and playerY == elkY and facing == elkFacing and not hasKey1 then
+    elkHead()
+  end
+  if playerX == garlicX and playerY == garlicY and facing == garlicFacing and not hasGarlic then
+    garlic()
+  end
+  if playerX == vampireX and playerY == vampireY and facing == vampireFacing then
+    vampire()
+  end
+  if playerX == stairCaseX and playerY == stairCaseY then
+    ascendToLevel2()
+  end
+end
+
+function elkHead()
+  --message about hidden key
+  hasKey1 = true
+end
+
+function garlic()
+  if hasKey1 then
+    --message about finding some garlic in the stock cabinet
+    hasGarlic = true
+  else
+    --message: the stock cabinet is locked. It seems a key is needed to open it
+  end
+end
+
+function vampire()
+  if hasGarlic then
+    --message about the vampire fleeing
+    --remove vampire tile
+  else
+    --message about the vampire blocking the way
+    --move player back one tile
+  end
+end
+
+function ascendToLevel2()
+  --load level 2
+end
+
+-- benötigte globale Variablen: elkX, elkY, elkFacing, garlicX, garlicY, garlicFacing, vampireX, vampireY, vampireFacing, stairCaseX, stairCaseY,
+-- hasKey1 = false, hasGarlic = false
+
+function triggerEventsLevel2()
   if playerX == bookX and playerY == bookY and facing == bookFacing then
     readNote()
   end
-  if playerX == keyX and playerY == keyY and facing == keyFacing and not hasKey then
+  if playerX == keyX and playerY == keyY and facing == keyFacing and not hasKey2 then
     ripKeyOut()
   end
   if playerX == closetX and playerY == closetY and facing == closetFacing and not closetDoorOpen then
@@ -16,17 +62,20 @@ function triggerEvents()
   end
 end
 
+-- benötigte globale Variablen: bookX, bookY, bookFacing, keyX2, keyY2, keyFacing2, closetX, closetY, closetFacing, atticX, atticY,
+-- ladderX, ladderY, ladderFacing, hasKey2 = false, hasLadder = false, closetDoorOpen = false
+
 function readNote()
   --show note
 end
 
 function ripKeyOut()
   --show disturbing Message
-  hasKey = true
+  hasKey2 = true
 end
 
 function closetDoor()
-  if hasKey then
+  if hasKey2 then
     --show Message: The door is now unlocked
     --swtich door tile to open door tile
     closetDoorOpen = true
