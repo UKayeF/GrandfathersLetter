8b88c98078b8c714b5fa48cdb87bc4b7d5b140d9
Racorror <paul.fluck@t-online.de>
2017-10-29 03:28:43 +0100

    Narrative improvements and level 1 map correction

--- src/eventTrigger.lua
+++ src/eventTrigger.lua
@@ -15,30 +15,34 @@ function triggerEventsLevel1()
 end
 
 function elkHead()
-  msg("You investigate the stuffed moose head on the wall. In a niche behind it you find a key")
+  a = "You investigate the stuffed moose head on the wall. Although you are sure that most of his Stories were fairytails, you must admit:\n"
+  b = "his hunting skills were always extraordinary. After a thorough examination you find a key in a niche behing the moose head."
+  msg( a..b )
     hasKey1 = love.keyboard.isScancodeDown("e")
 end
 
 function garlic()
   if hasKey1 then
-    msg("You use the key you found to open the storage cabinet. Inside There is some garlic. You take it with you.")
+    a = "You use the key that you found behind the stuffed moose head to open the storage cabinet. "
+    b = "Inside there are some cloves of garlic. for some reason you decide take them with you."
+    msg( a..b )
     hasGarlic = love.keyboard.isScancodeDown("e")
   else
-    msg("The stock cabinet is locked. It seems a key is needed to open it.")
+    msg("You try to open the stock cabinet. Unfortunately it is locked. It seems a key is needed to open it.")
   end
 end
 
 function vampire()
   if hasGarlic then
-    msg("The vampire flees in terror due to the stink of the garlic you are holding.")
+    msg("The vampire flees in terror due to the stink of the garlic you are holding. The way upstairs is now free!")
     vampireObj.isDead = love.keyboard.isScancodeDown("e")
   else
-    msg("A fearsome vampire is blocking the Way. There is no way through.")
+    msg("A fearsome vampire is blocking the way. There is no way through. Maybe you can dispel him somhow...")
   end
 end
 
 function ascendToLevel2()
-  msg("You have completed Level 1! You badass mofo!")
+  msg("You have finally reached the staircase. Hooray! Only one more story is between you and the attic.")
   level1Completed = love.keyboard.isScancodeDown("e")
 end
 
@@ -62,12 +66,16 @@ end
 
 function readNote()
   if inRange(1, noteObj) then
-    msg("You look through the bookshelf and find a note inside your grandfathers favorite book. It reads: Do not dwell in the past. Rip it apart in order to become a new man.")
+    a = "You look through the bookshelf and find a note inside your grandfathers favorite book, the ways of a true adventurer. "
+    b = "The note reads: Do not dwell in the past. Sometimes you need to let go or even destroy, what lies behind you in order to become a new man."
+    msg( a..b )
   end
 end
 
 function ripKeyOut()
-  msg("In an unexpected explosion of Rage, you rip apart the doll that you loved as a child. Inside you find another key.")
+  a = "You find your old doll that you used to play with as a child. You got a feeling that there is something inside it. "
+  b = "and indeed: After taking apart your once beloved doll, you hold another key in your hands."
+  msg( a..b )
   hasKey2 = love.keyboard.isScancodeDown("e")
 end
 
@@ -76,12 +84,12 @@ function closetDoor()
     msg("You open the door with the second key you got. Behind it seems to be a small closet")
     closetDoorObj.open = love.keyboard.isScancodeDown("e")
   elseif inRange(1, closetDoorObj) then
-    msg("The door is locked. It seems a key is needed to open it")
+    msg("This door is locked. It seems another key is needed to open it. Let the search begin anew!")
   end
 end
 
 function acquireLadder()
-  msg("You find a ladder an take it with you")
+  msg("You find a ladder and decide to take it with you, because: why not?")
   hasLadder = love.keyboard.isScancodeDown("e")
 end
 
